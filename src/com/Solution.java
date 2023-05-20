d874e73b76b8d8f4bb82b751e74b08e3e1854d81
merinjob <merinjob050@gmail.com>
2023-05-21 01:14:26 +0530

    git commit -m

--- src/com/Solution.java
+++ src/com/Solution.java
@@ -14,62 +14,84 @@ public class Solution {
 		System.out.println("Enter the Quantity of Product C:");
 		float cquan = scan.nextInt();
 		float csubtotal = 50 * cquan;
+
 		float sub_total = asubtotal + bsubtotal + csubtotal;
 		float cart_total = sub_total;
 		float total_quantity = aquan + bquan + cquan;
-		float discount1 = 0;
-		float discount2 = 0;
-		float discount3 = 0;
-		float discount4 = 0;
-		float discount = 0;
+		float discount1 = 0, discount2 = 0, discount3 = 0, discount4 = 0, discount = 0;
+
 		String name1 = "flat_10_discount";
 		String name2 = "bulk_5_discount";
 		String name3 = "bulk_10_discount";
 		String name4 = "tiered_50_discount";
-		String name = "No Discounts Applicable";
+		String name = "";
 		scan.close();
+
 		Discount d = new DiscountImpl();
 
-		// Discounts 
-		
+		// Available Discounts
+		// Discount 1
+
 		if (cart_total > 200) {
 			discount1 = d.flat_10_discount(cart_total);
 
 		}
+
+		// Discount 2
+
 		if (aquan > 10 || bquan > 10 || cquan > 10) {
-			float subtotal = 0;
+			float d1 = 0, d2 = 0, d3 = 0;
 			if (aquan > 10) {
-				subtotal = asubtotal;
-			} else if (bquan > 10) {
-				subtotal = bsubtotal;
-			} else {
-				subtotal = csubtotal;
+				d1 = d.bulk_5_discount(asubtotal);
 			}
-			discount2 = d.bulk_5_discount(subtotal);
+			if (bquan > 10) {
+				d2 = d.bulk_5_discount(bsubtotal);
+			}
+			if (cquan > 10) {
+				d3 = d.bulk_5_discount(csubtotal);
+			}
+
+			if (d1 > d2 && d1 > d3) {
+				discount2 = d1;
+			} else if (d2 > d1 && d2 > d3) {
+				discount2 = d2;
+			} else {
+				discount2 = d3;
+			}
 		}
+
+		// Discount 3
+
 		if (aquan > 20 || bquan > 20 || cquan > 20) {
 			discount3 = d.bulk_10_discount(cart_total);
 		}
 
-		if (total_quantity > 30 && (aquan > 15 || bquan > 15 || cquan > 15)) {
-			float quantity = 0;
-			float price = 0;
-			if (aquan > 15) {
-				quantity = aquan - 15;
-				price = quantity * 20;
-				cart_total = price;
-			} else if (bquan > 15) {
-				quantity = bquan - 15;
-				price = quantity * 40;
-				cart_total = price;
-			} else {
-				quantity = cquan - 15;
-				price = quantity * 50;
-				cart_total = price;
-			}
-			discount4 = d.tiered_50_discount(cart_total);
+		// Discount 4
 
+		if (total_quantity > 30 && (aquan > 15 || bquan > 15 || cquan > 15)) {
+			float dis1 = 0, dis2 = 0, dis3 = 0;
+			if (aquan > 15) {
+				cart_total = (aquan - 15)*20;
+				dis1=d.tiered_50_discount(cart_total);
+			} if (bquan > 15) {
+				cart_total = (bquan - 15)*40;
+				dis2=d.tiered_50_discount(cart_total);
+			} if(cquan>15) {
+				cart_total = (cquan - 15)*50;
+				dis3=d.tiered_50_discount(cart_total);
+			}
+			
+			if (dis1 > dis2 && dis1 > dis3) {
+				discount4 = dis1;
+			} else if (dis2 > dis1 && dis2 > dis3) {
+				discount4 = dis2;
+			} else {
+				discount4 = dis3;
+			}
 		}
+
+		
+		// Finding most favourable discounts
 
 		if (discount1 > discount2) {
 			if (discount1 > discount3) {
@@ -80,26 +102,40 @@ public class Solution {
 					discount = discount4;
 					name = name4;
 				}
-			} else {
+			} else if (discount3 > discount4) {
 				discount = discount3;
 				name = name3;
+			} else {
+				discount = discount4;
+				name = name4;
 			}
+		} else if (discount2 > discount3) {
+			if (discount2 > discount4) {
+				discount = discount2;
+				name = name2;
+			} else {
+				discount = discount4;
+				name = name4;
+			}
+		} else if (discount3 > discount4) {
+			discount = discount3;
+			name = name3;
 		} else {
-			discount = discount2;
-			name = name2;
+			discount = discount4;
+			name = name4;
 		}
 
-		// gift wrap fee
+		// Gift wrap fee
 
 		float gift_wrap_fee = total_quantity * 1;
 
 		// Shipping fee
 
-				
-		int packages = (int) total_quantity / 10 + ((total_quantity % 10)==0? 0:1);
-		
+		int packages = (int) total_quantity / 10 + ((total_quantity % 10) == 0 ? 0 : 1);
 		float shipping_fee = packages * 5;
 		float total = sub_total - discount + gift_wrap_fee + shipping_fee;
+
+		// Output
 
 		System.out.println("Product A : " + aquan + "-->" + asubtotal);
 		System.out.println("Product B : " + bquan + "-->" + bsubtotal);
@@ -107,9 +143,9 @@ public class Solution {
 		System.out.println("Subtotal : " + sub_total);
 		if (discount != 0) {
 			System.out.println("Discount name : " + name + ", Discount Amount : " + discount);
-		}
-		else {
-			System.out.println("Discount name : Discount Amount Not Applicable");
+		} else {
+			System.out.println("No Discounts Applicable");
+			System.out.println("Discount Amount : " + discount);
 		}
 		System.out.println("Shipping Fee : " + shipping_fee);
 		System.out.println("Gift Wrap Fee : " + gift_wrap_fee);
